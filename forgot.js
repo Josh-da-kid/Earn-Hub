b053d83378ae45c21ed21069e850dd8f978a6d6f
Josh-da-kid <carmenjosh84@gmail.com>
2025-01-14 21:23:36 +0100

    Update and rename script.js to forgot.js

---
+++ forgot.js
@@ -0,0 +1,5 @@
+let info = document.getElementById('info')
+let msg = document.getElementById('msg')
+info.addEventListener('click', function() {
+    msg.textContent = "It happens to the best of us! Just input the email address you used in signing up to the account to wish to retrieve and follow the simplesteps to reset your password."
+})
