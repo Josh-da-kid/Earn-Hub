f1e00f763431a449a5abf9d8560574c2da3e08bd
Josh-da-kid <carmenjosh84@gmail.com>
2025-01-14 21:36:37 +0100

    Update forgot.js

--- forgot.js
+++ forgot.js
@@ -3,3 +3,6 @@ let msg = document.getElementById('msg')
 info.addEventListener('click', function() {
     msg.textContent = "It happens to the best of us! Just input the email address you used in signing up to the account to wish to retrieve and follow the simplesteps to reset your password."
 })
+resetButton.addEventListener("click", function() {
+    msg.textContent = ""
+})
