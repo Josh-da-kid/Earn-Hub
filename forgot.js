2e674237d558ecfec17981cbad51d639ee8a9f22
Josh-da-kid <carmenjosh84@gmail.com>
2025-01-14 21:45:21 +0100

    Update forgot.js

--- forgot.js
+++ forgot.js
@@ -1,9 +1,16 @@
 let info = document.getElementById('info')
 let msg = document.getElementById('msg')
 let resetButton = document.getElementById('rp-btn')
-info.addEventListener('click', function() {
+info.addEventListener('dblclick', function() {
     msg.textContent = "It happens to the best of us! Just input the email address you used in signing up to the account to wish to retrieve and follow the simplesteps to reset your password."
 })
+
+info.addEventListener('click', function() {
+    msg.textContent = ""
+
+
+})
+
 resetButton.addEventListener("click", function() {
     msg.textContent = ""
 })
