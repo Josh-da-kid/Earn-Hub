157a2b46e6e87f48b3e6e0a6f9d7cb94f68facb8
Josh-da-kid <carmenjosh84@gmail.com>
2025-01-14 22:10:20 +0100

    Update forgot.js

--- forgot.js
+++ forgot.js
@@ -1,16 +1,19 @@
-let info = document.getElementById('info')
-let msg = document.getElementById('msg')
-let resetButton = document.getElementById('rp-btn')
-info.addEventListener('dblclick', function() {
-    msg.textContent = "It happens to the best of us! Just input the email address you used in signing up to the account to wish to retrieve and follow the simplesteps to reset your password."
-})
-
-info.addEventListener('click', function() {
-    msg.textContent = ""
+let info = document.getElementById('info');
+let msg = document.getElementById('msg');
+let resetButton = document.getElementById("rp-btn");
 
 
-})
+const messageText = "It happens to the best of us! Just input the email address you used in signing up to the account you wish to retrieve and follow the simple steps to reset your password.";
 
-resetButton.addEventListener("click", function() {
-    msg.textContent = ""
-})
+info.addEventListener('click', function () {
+    
+    if (msg.textContent.trim() === "") {
+        msg.textContent = messageText; 
+    } else {
+        msg.textContent = ""; 
+    }
+});
+
+resetButton.addEventListener("click", function () {
+    msg.textContent = ""; 
+});
