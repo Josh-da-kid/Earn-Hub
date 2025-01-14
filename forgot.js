258a94ba7757ba01be8270dd0f86238c4cbd1f3a
Josh-da-kid <carmenjosh84@gmail.com>
2025-01-14 21:39:32 +0100

    Update forgot.js

--- forgot.js
+++ forgot.js
@@ -1,5 +1,6 @@
 let info = document.getElementById('info')
 let msg = document.getElementById('msg')
+let resetButton = document.getElementById('rp-btn')
 info.addEventListener('click', function() {
     msg.textContent = "It happens to the best of us! Just input the email address you used in signing up to the account to wish to retrieve and follow the simplesteps to reset your password."
 })
